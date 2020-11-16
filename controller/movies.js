5600d146b5c12e399592dfddec9eed1feaf0bd99
Clarissa  Barrow <clarissabarrow@gmail.com>
2020-11-16 00:09:13 -0500

    completed

--- controller/movies.js
+++ controller/movies.js
@@ -1,24 +1,58 @@
 const movies = require('../movies');
 
+// general movie search query
+const searchMovies = (request, response) => {
+  const { query } = request.params;
+  const genericQuery = query.toLowerCase();
+
+  //call movie searches
+  const byTitle = filterMovieByTitle(genericQuery);
+  const byDirector = filterMovieByDirector(genericQuery);
+  const searchResults = byTitle.concat(byDirector);
+  return response.send(searchResults);
+};
+
 const getAllMovies = (request, response) => {
   return response.send(movies);
 };
 
-const getMovieByTitle = (request, response) => {
-  const { title } = request.params;
+const filterMovieByTitle = (title) => {
+  const movieTitle = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(title.toLowerCase())
+  );
+  return movieTitle;
+};
 
-  const movieTitle = movies.filter((movie) => movie.title == title);
-
-  return response.send(movieTitle);
+const filterMovieByDirector = (directors) => {
+  const movieDirector = movies.filter((movie) => {
+    for (let i = 0; i < movie.directors.length; i++) {
+      const director = movie.directors[i].toLowerCase();
+      if (director.includes(directors.toLowerCase())) {
+        return true;
+      }
+    }
+  });
+  return movieDirector;
 };
 
 const getMovieByDirector = (request, response) => {
-  const { directors } = request.params;
-
-  const movieDirector = movies.filter(
-    (movie) => movie.directors.length == directors
-  );
-
-  return response.send(movieDirector);
+  const { query } = request.params;
+  return response.send(filterMovieByDirector(query));
 };
-module.exports = { getAllMovies, getMovieByDirector, getMovieByTitle };
+
+const getMovieByTitle = (request, response) => {
+  const { query } = request.params;
+  return response.send(filterMovieByTitle(query));
+};
+
+const createNewMovie = (request, response) => {
+  return response.send(request.body);
+};
+
+module.exports = {
+  getAllMovies,
+  getMovieByDirector,
+  getMovieByTitle,
+  searchMovies,
+  createNewMovie,
+};
--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const movies = require('./movies');
+const bodyParser = require('body-parser');
 const {
   getAllMovies,
+  searchMovies,
+  createNewMovie,
   getMovieByTitle,
   getMovieByDirector,
 } = require('./controller/movies');
+const { response } = require('express');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/', getAllMovies);
+app.get('/', (req, res) => {
+  res.send('Movies');
+});
+//gets all movies
+app.get('/movies', getAllMovies);
 
-app.get('/:title', getMovieByTitle);
-app.get('/:director', getMovieByDirector);
+//get movies by search query whether director or title) instructions are not super clear on whether or not you need 2 separte routes.
+app.get('/movies/:query', searchMovies);
+
+// create new movie
+app.post('/movies', createNewMovie);
+// curl req to confirm post to endpoint:
+//curl -X POST -H "Content-Type: application/json" --data '{ "title": "Cabin in the Woods", "directors": ["Drew Goddard"], "releaseDate": "04/13/2012", "rating": "R","runTime": "95 mins", "genres": ["Horror"]}' http://localhost:8000/movies
+
+//get movies by title
+app.get('/movies/title/:query', getMovieByTitle);
+
+//get movies by director
+app.get('/movies/director/:query', getMovieByDirector);
 app.listen(8000, () => {
   console.log('Listening on port 8000 .....');
 });
